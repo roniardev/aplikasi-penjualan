39d1767275ded6de166401b3720e3165a07023cc
roniardiyanto <roni.ardiyanto@koding.sch.id>
2020-01-20 17:48:10 +0700

    menambahkan user styles

---
+++ src/pages/private/settings/styles/user.js
@@ -0,0 +1,11 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme =>({
+    userSettings: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: 300
+    }
+}))
+
+export default useStyles;
