c12ebdb234a54f54d04e85d4c55a05602cea9e35
roniardiyanto <roni.ardiyanto@koding.sch.id>
2020-01-29 12:17:43 +0700

    Menambahkan form update password

--- src/pages/private/settings/user.js
+++ src/pages/private/settings/user.js
@@ -14,10 +14,13 @@ function UserSettings() {
     const {user} = useFirebase();
     const displayNameRef = useRef();
     const emailRef = useRef();
+    const passwordRef = useRef();
     const [isSubmitting, setSubmitting] = useState(false);
     const [error, setError] = useState({
         displayName:'',
-        email: ''
+        email: '',
+        password: ''
+        
     })
     const {enqueueSnackbar} = useSnackbar();
     const saveDisplayName = async e => {
@@ -88,12 +91,44 @@ function UserSettings() {
         enqueueSnackbar(`Email verifikasi telah dikirimkan ke ${emailRef.current.value}`, {variant: 'success'})
         setSubmitting(false);
     }
+    const updatePassword = async e => {
+        const password = passwordRef.current.value;
+        if(!password) {
+            setError({
+                password: "Password wajib diisi"
+            })
+        }else{
+            setSubmitting(true)
+            try {
+                await user.updatePassword(password);
+                enqueueSnackbar("Password berhasil diupdate", {variant: 'success'})
+            } catch (error) {
+                let errorPassword = '';
+                switch (error.code) {
+                    case 'auth/weak-password':
+                        errorPassword = "Password terlalu lemah";
+                        break;
+                    case 'auth/requires -recent-login':
+                        errorPassword = 'silahkan login ulang untuk memperbarui Password'
+                        break;
+                        
+                    default:
+                        errorPassword = "Terjadi kesalahan, silahkan ulangi lagi"
+                        break;
+                }
+                setError({
+                    password: errorPassword
+                })
+            }
+            setSubmitting(false)
+        }
+    }
     return <div className={classes.userSettings}>
         <TextField margin="normal" id="displayName" name="displayName" label="Nama" inputProps={{
             ref: displayNameRef,
             onBlur: saveDisplayName
         }} disabled={isSubmitting} defaultValue={user.displayName} helperText={error.displayName} error={error.displayName? true:false} />
-        <TextField margin="normal" id="email" name="email" label="Email" defaultValue={user.email} inputProps={{
+        <TextField type="email" margin="normal" id="email" name="email" label="Email" defaultValue={user.email} inputProps={{
             ref: emailRef,
             onBlur: updateEmail
         }} helperText={error.email} error={error.email?true:false} disabled={isSubmitting} />
@@ -102,6 +137,10 @@ function UserSettings() {
         :
         <Button onClick={sendEmailVerification} disabled={isSubmitting} variant="outlined">Kirim Email Verifikasi</Button>
          }
+         <TextField margin="normal" id="password" name="password" label="Password baru" type="password" inputProps={{
+             ref: passwordRef,
+             onBlur: updatePassword
+         }} disabled={isSubmitting} autoComplete="new-password" helperText={error.password} error={error.password? true:false}></TextField>
     </div>
 };
 
