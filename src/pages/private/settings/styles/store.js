98a8bdc11682960ea69c7946f74448e491be1cd6
Roni Ardiyanto <roon@disroot.org>
2020-02-03 16:09:22 +0700

    membuat style pengaturan toko

---
+++ src/pages/private/settings/styles/store.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  storeSettings: {
+    display: "flex",
+    flexDirection: "column",
+    width: 300
+  }
+}));
+
+export default useStyles;
