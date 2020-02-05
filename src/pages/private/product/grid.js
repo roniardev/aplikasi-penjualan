19f55a890f74e7d7874eac964de4b63ae43570ec
Roni Ardiyanto <roon@disroot.org>
2020-02-05 11:12:01 +0700

    menambah fitur halaman grid

--- src/pages/private/product/grid.js
+++ src/pages/private/product/grid.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+//material-ui
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+//useStyles
+import useStyles from "./styles/grid";
+//page component
+import AddDialog from "./add";
 
 function GridProduct() {
-    return <h1>Grid Product</h1>
+  const [openAddDialog, setOpenAddDialog] = useState(false);
+  const classes = useStyles();
+  return (
+    <>
+      <h1>Halaman Grid Product</h1>;
+      <Fab
+        className={classes.fab}
+        color="primary"
+        onClick={e => {
+          setOpenAddDialog(true);
+        }}
+      >
+        <AddIcon />
+      </Fab>
+      <AddDialog
+        open={openAddDialog}
+        handleClose={() => {
+          setOpenAddDialog(false);
+        }}
+      />
+    </>
+  );
 }
 
 export default GridProduct;
